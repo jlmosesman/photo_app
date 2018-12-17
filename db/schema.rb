11f82ed12431fb2e3247cc7253f7087bb62fd856
Jonathan Mosesman <jonathanmosesman@gmail.com>
2018-12-17 16:05:21 -0600

    generate User model, add validations, passwords, user rspecs

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_12_220939) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+ActiveRecord::Schema.define(version: 2018_12_17_214154) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -31,5 +28,14 @@ ActiveRecord::Schema.define(version: 2018_12_12_220939) do
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
-  add_foreign_key "comments", "articles"
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,70 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it "validates user data" do
+    expect(User.new.valid?).to eq(false)
+
+    user = create(:user)
+
+    expect(user.valid?).to eq(true)
+  end
+
+  it "validates email" do
+    user = User.new(
+      first_name: 'Bob',
+      last_name: 'Smith',
+      email: 'invalid@email,com',
+      password: 'password',
+      password_confirmation: 'password'
+    )
+    user.validate
+
+    expect(user.errors.first).to eq([:email, "is invalid"])
+  end
+
+  it "should not allow duplicate user emails" do
+    user1 = User.create(
+      first_name: 'Bob',
+      last_name: 'Smith',
+      email: 'bob@email.com',
+      password: 'password',
+      password_confirmation: 'password'
+    )
+
+    user2 = User.create(
+      first_name: 'Bob',
+      last_name: 'Anderson',
+      email: 'Bob@email.com', # same email
+      password: 'password',
+      password_confirmation: 'password'
+    )
+
+    expect(user2.valid?).to eq(false)
+  end
+
+  it "can't have blank password" do
+    user = User.new(
+      first_name: 'Bob',
+      last_name: 'Smith',
+      email: 'user@email.com',
+      password: ' ',
+      password_confirmation: ' '
+    )
+    user.validate
+
+    expect(user.errors.first).to eq([:password, "can't be blank"])
+  end
+
+  it "requires 6 minimum length of password" do
+    user = User.new(
+      first_name: 'Bob',
+      last_name: 'Smith',
+      email: 'user@email.com',
+      password: 'pas',
+      password_confirmation: 'pas'
+    )
+    user.validate
+
+    expect(user.errors.first).to eq([:password, "is too short (minimum is 6 characters)"])
+  end
+end
